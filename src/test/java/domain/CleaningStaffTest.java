4250e09b4f502ff4fe3fbb2d12a1e9d46f52077e
geonHee <corqkrtk3@naver.com>
2019-05-27 16:06:47 +0900

    [TEST] 요구사항별 테스트케이스 작성

---
+++ src/test/java/domain/CleaningStaffTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.junit.Test;
+
+public class CleaningStaffTest {
+    @Test
+    public void 손님이_나가면_청소_시작() {
+
+    }
+
+    @Test
+    public void 청소_중에는_테이블_사용불가() {
+
+    }
+
+    @Test
+    public void 청소_완료_후_테이블_사용가능() {
+
+    }
+}
---
+++ src/test/java/domain/CustomerTest.java
@@ -0,0 +1,30 @@
+package domain;
+
+import org.junit.Test;
+
+public class CustomerTest {
+    @Test
+    public void 손님이_입장했는데_테이블_공간부족으로_대기() {
+
+    }
+
+    @Test
+    public void 테이블에_착석_후_요리대기() {
+
+    }
+
+    @Test
+    public void 손님이_1시간동안_기다리면_가게를_나감() {
+
+    }
+
+    @Test
+    public void 식사시작() {
+
+    }
+
+    @Test
+    public void 식사_완료후_퇴장() {
+
+    }
+}

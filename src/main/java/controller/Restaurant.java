db12f7758fd386045c5003d4eb8dd252eeb63e06
geonHee <corqkrtk3@naver.com>
2019-05-31 22:50:13 +0900

    [REFACTOR] Restaurant Controller 메서드 분리

--- src/main/java/controller/Restaurant.java
+++ src/main/java/controller/Restaurant.java
@@ -16,6 +16,7 @@ public class Restaurant {
     private Tables tables;
     private Cooks cooks;
     private Cleaners cleaners;
+    private int numberOfCustomer;
 
     public Restaurant(MeasureTime measureTime, Cooks cooks, Cleaners cleaners, Tables tables) {
         this.measureTime = measureTime;
@@ -23,69 +24,94 @@ public class Restaurant {
         this.cooks = cooks;
         this.cleaners = cleaners;
         this.tables = tables;
+        numberOfCustomer = 0;
     }
 
     public int open() {
-        int numberOfCustomer = 0;
         while (measureTime.isOpening()) {
             visitGuests();
             measureTime.addOneMinute();
-            while (true) {
-                Table maybeTable = tables.sitOnTable();
-                if (maybeTable == null) {
-                    break;
-                }
-
-                if (waitingRoom.hasNotGuest()) {
-                    break;
-                }
-                maybeTable.sitGuest(waitingRoom.leaveGuest());
-            }
+            // 대기손님 테이블로 이동
+            designateTable();
 
             // 테이블에 앉아있는 손님에게 요리사 배정
-            List<Table> tablesOfSitGuestAndNoCook = tables.tablesOfSitGuestAndNoCook();
-            for (Table table : tablesOfSitGuestAndNoCook) {
-                Cook waitCook = cooks.findWaitCook();
-                if (waitCook == null) {
-                    break;
-                }
-                table.setCook(waitCook);
-                table.startCook(); // 손님이 테이블에 앉으면 요리사는 바로 요리를 한다. (소요시간 4분)
-            }
+            designateCook();
 
             // 손님에게 식사를 건낸다.
-            for (Table table : tables.getTables()) {
-                table.giveFood();
-            }
+            giveFoodToGuest();
 
             // 먹고 나간 손님
-            for (Table table : tables.getTables()) {
-                if (table.isFinishEating()) {
-                    numberOfCustomer++;
-                }
-            }
+            leaveRestaurant();
 
             // 손님이 나가면 청소 스텝은 테이블을 정리합니다. (소요시간 3분)
-            for (Table table : tables.getTables()) {
-                if (!table.isClean()) {
-                    Cleaner waitCleaner = cleaners.findWaitCleaner();
-                    if (waitCleaner == null) {
-                        break;
-                    }
-                    table.setCleaner(waitCleaner);
-                    table.startCleaner();
-                }
-            }
+            designateCleaner();
 
             // 테이블 정리가 완료된 직후 다시 테이블을 사용할 수 있다.
-            for (Table table : tables.getTables()) {
-                table.finishClean();
-            }
+            completeCleaning();
 
             waitingRoom.addWaitingMinuteTime(); // 대기하는 사람들 1초 추가
             tables.addOneMinute(); // 테이블에 앉은 손님과 테이블 담당자들 1초 추가
         }
         return numberOfCustomer;
+    }
+
+    private void completeCleaning() {
+        for (Table table : tables.getTables()) {
+            table.finishClean();
+        }
+    }
+
+    private void designateCleaner() {
+        for (Table table : tables.getTables()) {
+            if (!table.isClean()) {
+                Cleaner waitCleaner = cleaners.findWaitCleaner();
+                if (waitCleaner == null) {
+                    break;
+                }
+                table.setCleaner(waitCleaner);
+                table.startCleaner();
+            }
+        }
+    }
+
+    private void leaveRestaurant() {
+        for (Table table : tables.getTables()) {
+            if (table.isFinishEating()) {
+                numberOfCustomer++;
+            }
+        }
+    }
+
+    private void giveFoodToGuest() {
+        for (Table table : tables.getTables()) {
+            table.giveFood();
+        }
+    }
+
+    private void designateTable() {
+        while (true) {
+            Table maybeTable = tables.sitOnTable();
+            if (maybeTable == null) {
+                break;
+            }
+
+            if (waitingRoom.hasNotGuest()) {
+                break;
+            }
+            maybeTable.sitGuest(waitingRoom.leaveGuest());
+        }
+    }
+
+    private void designateCook() {
+        List<Table> tablesOfSitGuestAndNoCook = tables.tablesOfSitGuestAndNoCook();
+        for (Table table : tablesOfSitGuestAndNoCook) {
+            Cook waitCook = cooks.findWaitCook();
+            if (waitCook == null) {
+                break;
+            }
+            table.setCook(waitCook);
+            table.startCook(); // 손님이 테이블에 앉으면 요리사는 바로 요리를 한다. (소요시간 4분)
+        }
     }
 
     private void visitGuests() {
